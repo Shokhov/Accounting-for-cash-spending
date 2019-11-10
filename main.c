de9d37dd2610b18d666956d8921458b71a80ae4f
Shokhov <dofi2014@yandex.ru>
2019-11-11 02:51:18 +0300

    Fix: bug and finished interface(0.9)

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 void Menu();
 
+void add_record_to_table();
 void Add_Employees();
 void Add_Nomenclature();
 void Add_Stock();
@@ -18,6 +19,14 @@ void Add_Management();
 void Add_Deliveries();
 void Add_Leftovers();
 
+void change_record_to_tabel();
+void Change_Employees(int rez_out);
+void Change_Nomenclature(int rez_out);
+void Change_Deliveries(int rez_out);
+
+void remove_record_to_table();
+
+void out_record_to_table();
 
 int test_sravnenie(int kod0, int kod1, char *name0, char *name1, int age0, int age1){
     if(kod0 != kod1) return 1;
@@ -129,27 +138,29 @@ void Menu(){
                "1. Добавить запись в таблицу\n"
                "2. Изменить запись в таблице\n"
                "3. Удалить запись в таблице\n"
-               "4. Проверить БД на ошибки\n"
+               "4. Вывести все записи таблицы\n"
+               "5. Проверить БД на ошибки\n"
                "0. Выход\n"
                "Выберите пункт меню: ");
 
         rez=getchar();while(getchar() != '\n'); /*Сбрасываем все символы кроме первого*/
 
         switch (rez) {
-            case '1': add_record_to_table(); break;
-            case '2': break;
-            case '3': break;
-            case '4': break;
-        case '0': rez=0; break;
+        case '1': add_record_to_table(); break;
+        case '2': change_record_to_tabel(); break;
+        case '3': remove_record_to_table(); break;
+        case '4': out_record_to_table(); break;
+        case '5': break;
+        case '0': return;
         }
-    }while(rez);
+    }while(1);
     puts("\nВыход\n");
 }
 
 void add_record_to_table(){
     char rez;
     /*Вывод всех файлов*/
-    puts("\nВсе таблицы:");
+    puts("\nДобавление записи в таблицах:\nВсе таблицы:");
     for(int i=0;i<7;i++){
         printf("%i. %s\n",i+1,BD_list_fails[i]);
     }
@@ -175,11 +186,11 @@ void Add_Employees(){
     for(int i=0;i<5;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&E_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("имя: "); scanf("%s",&E_temp.name);while(getchar() != '\n'); break;
-        case 2: printf("возраст: "); scanf("%i",&E_temp.age);while(getchar() != '\n'); break;
-        case 3: printf("должность: "); scanf("%s",&E_temp.position);while(getchar() != '\n');  break;
-        case 4:  printf("оклад: "); scanf("%i",&E_temp.salary);while(getchar() != '\n'); break;
+        case 0: printf("код: "); scanf("%i",&E_temp.code);break;
+        case 1: printf("имя: "); scanf("%s",&E_temp.name); break;
+        case 2: printf("возраст: "); scanf("%i",&E_temp.age);break;
+        case 3: printf("должность: "); scanf("%s",&E_temp.position); break;
+        case 4: printf("оклад: "); scanf("%i",&E_temp.salary); break;
         }
     }
     BD_add(BD_list_fails[0],&E_temp,E_size);
@@ -191,9 +202,9 @@ void Add_Nomenclature(){
     for(int i=0;i<3;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&N_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("наименование: "); scanf("%s",&N_temp.name);while(getchar() != '\n'); break;
-        case 2: printf("процентную надбавку: "); scanf("%i",&N_temp.markup_percentage);while(getchar() != '\n'); break;
+        case 0: printf("код: "); scanf("%i",&N_temp.code);break;
+        case 1: printf("наименование: "); scanf("%s",&N_temp.name);break;
+        case 2: printf("процентную надбавку: "); scanf("%i",&N_temp.markup_percentage);break;
         }
     }
     BD_add(BD_list_fails[1],&N_temp,N_size);
@@ -205,9 +216,9 @@ void Add_Stock(){
     for(int i=0;i<3;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&S_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("наименование: "); scanf("%s",&S_temp.name);while(getchar() != '\n'); break;
-        case 2: printf("вид склада: "); scanf("%s",&S_temp.type_of_warehouse);while(getchar() != '\n'); break;
+        case 0: printf("код: "); scanf("%i",&S_temp.code); break;
+        case 1: printf("наименование: "); scanf("%s",&S_temp.name);break;
+        case 2: printf("вид склада: "); scanf("%s",&S_temp.type_of_warehouse);break;
         }
     }
     BD_add(BD_list_fails[2],&S_temp,S_size);
@@ -219,8 +230,8 @@ void Add_Counterparties(){
     for(int i=0;i<2;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&C_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("наименование: "); scanf("%s",&C_temp.name);while(getchar() != '\n'); break;
+        case 0: printf("код: "); scanf("%i",&C_temp.code);break;
+        case 1: printf("наименование: "); scanf("%s",&C_temp.name);break;
         }
     }
     BD_add(BD_list_fails[3],&C_temp,C_size);
@@ -232,10 +243,10 @@ void Add_Management(){
     for(int i=0;i<4;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&M_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("дата: "); scanf("%i",&M_temp.data);while(getchar() != '\n'); break;
-        case 2: printf("код сотрудника: "); scanf("%i",&M_temp.code_employee);while(getchar() != '\n'); break;
-        case 3: printf("статус сотрудника: "); scanf("%i",&M_temp.status);while(getchar() != '\n');  break;
+        case 0: printf("код: "); scanf("%i",&M_temp.code);break;
+        case 1: printf("дата: "); scanf("%i",&M_temp.data); break;
+        case 2: printf("код сотрудника: "); scanf("%i",&M_temp.code_employee); break;
+        case 3: printf("статус сотрудника: "); scanf("%i",&M_temp.status);  break;
         }
     }
     BD_add(BD_list_fails[4],&M_temp,M_size);
@@ -247,11 +258,11 @@ void Add_Deliveries(){
     for(int i=0;i<5;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&D_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("дата: "); scanf("%i",&D_temp.data);while(getchar() != '\n'); break;
-        case 2: printf("код контрагента: "); scanf("%i",&D_temp.code_counterparties);while(getchar() != '\n'); break;
-        case 3: printf("код склада: "); scanf("%i",&D_temp.code_stock);while(getchar() != '\n');  break;
-        case 4:  printf("сумма: "); scanf("%i",&D_temp.summa);while(getchar() != '\n'); break;
+        case 0: printf("код: "); scanf("%i",&D_temp.code);break;
+        case 1: printf("дата: "); scanf("%i",&D_temp.data); break;
+        case 2: printf("код контрагента: "); scanf("%i",&D_temp.code_counterparties); break;
+        case 3: printf("код склада: "); scanf("%i",&D_temp.code_stock);break;
+        case 4: printf("сумма: "); scanf("%i",&D_temp.summa);break;
         }
     }
     BD_add(BD_list_fails[5],&D_temp,D_size);
@@ -263,15 +274,238 @@ void Add_Leftovers(){
     for(int i=0;i<5;i++){
         printf("Введите ");
         switch (i) {
-        case 0: printf("код: "); scanf("%i",&L_temp.code);while(getchar() != '\n'); break;
-        case 1: printf("код номенклатуры: "); scanf("%i",&L_temp.code_nomenclature);while(getchar() != '\n'); break;
-        case 2: printf("код склада: "); scanf("%i",&L_temp.code_stock);while(getchar() != '\n'); break;
-        case 3: printf("количество: "); scanf("%i",&L_temp.quantity);while(getchar() != '\n');  break;
-        case 4:  printf("сумма: "); scanf("%i",&L_temp.summa);while(getchar() != '\n'); break;
+        case 0: printf("код: "); scanf("%i",&L_temp.code);break;
+        case 1: printf("код номенклатуры: "); scanf("%i",&L_temp.code_nomenclature);break;
+        case 2: printf("код склада: "); scanf("%i",&L_temp.code_stock);break;
+        case 3: printf("количество: "); scanf("%i",&L_temp.quantity);break;
+        case 4: printf("сумма: "); scanf("%i",&L_temp.summa);break;
         }
     }
     BD_add(BD_list_fails[6],&L_temp,L_size);
 }
+
+void change_record_to_tabel(){
+    char rez;
+    int rez_out, count;
+    printf("\nИзменение данных в таблице:\nТаблица для изменения:\n");
+    printf("1. %s\n2. %s\n6. %s\n",BD_list_fails[0],BD_list_fails[1],BD_list_fails[5]);
+
+    do{
+        printf("\nВыберите файл [1,2,6]: ");
+        rez=getchar();while(getchar() != '\n'); /*Сбрасываем все символы кроме первого*/
+
+        switch (rez) {
+        case '1':
+            count=0;
+            printf("\n|Номер|Код|Наименование|Возраст|Должность|Оклад|\n");
+            while(rez_out=BD_output(BD_list_fails[0],&E_temp,E_size)){ count++;
+                if(E_temp.code != -1) printf("|%5i|%3i|%12s|%7i|%9s|%5i|\n",rez_out,E_temp.code,E_temp.name,E_temp.age,E_temp.position,E_temp.salary);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            Change_Employees(rez_out-1);
+            break;
+        case '2':
+            count=0;
+            printf("\n|Номер|Код|Наименование| % |\n");
+            while(rez_out=BD_output(BD_list_fails[1],&N_temp,N_size)){ count++;
+                if(N_temp.code != -1) printf("|%5i|%3i|%12s|%3i|\n",rez_out,N_temp.code,N_temp.name,N_temp.markup_percentage);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            Change_Nomenclature(rez_out-1);
+            break;
+        case '6':
+            count=0;
+            printf("\n|Номер|Код| Дата |Контрагент|Склад|Сумма|\n");
+            while(rez_out=BD_output(BD_list_fails[5],&D_temp,D_size)){ count++;
+                if(D_temp.code != -1) printf("|%5i|%3i|%6i|%10i|%5i|%5i|\n",rez_out,D_temp.code,D_temp.data,D_temp.code_counterparties,D_temp.code_stock,D_temp.summa);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            Change_Deliveries(rez_out-1);
+            break;
+        case '0': return;
+        }
+    }while('1'!=rez && rez!='2' && rez!='6');
+}
+
+void Change_Employees(int rez_out){
+    BD_out(BD_list_fails[0],&E_temp,E_size,rez_out);
+    printf("\nИзменение записи в таблице:\n");
+    for(int i=0;i<2;i++){
+        printf("Измените данные ");
+        switch (i) {
+        case 0: printf("должности: "); scanf("%s",&E_temp.position);break;
+        case 1: printf("оклада: "); scanf("%i",&E_temp.salary);break;
+        }
+    }
+    BD_change(BD_list_fails[0],&E_temp,E_size,rez_out);
+}
+
+void Change_Nomenclature(int rez_out){
+    BD_out(BD_list_fails[1],&N_temp,N_size,rez_out);
+    printf("\nИзменение записи в таблице:\n");
+    for(int i=0;i<2;i++){
+        printf("Измените данные ");
+        switch (i) {
+        case 0: printf("наименования: "); scanf("%s",&N_temp.name);break;
+        case 1: printf("надбавки в процентах: "); scanf("%i",&N_temp.markup_percentage);break;
+        }
+    }
+    BD_change(BD_list_fails[1],&N_temp,N_size,rez_out);
+}
+
+void Change_Deliveries(int rez_out){
+    BD_out(BD_list_fails[5],&D_temp,D_size,rez_out);
+    printf("\nИзменение записи в таблице:\n");
+    for(int i=0;i<4;i++){
+        printf("Измените данные ");
+        switch (i) {
+        case 0: printf("даты: "); scanf("%i",&D_temp.data);break;
+        case 1: printf("кода контрагента: "); scanf("%i",&D_temp.code_counterparties);break;
+        case 2: printf("кода склада: "); scanf("%i",&D_temp.code_stock);break;
+        case 3: printf("суммы: "); scanf("%i",&D_temp.summa);break;
+        }
+    }
+    BD_change(BD_list_fails[5],&D_temp,D_size,rez_out);
+}
+
+void remove_record_to_table(){
+    char rez;
+    int rez_out, count;
+    printf("\nУдаление данных в таблице:\nДоступные таблицы:\n");
+    printf("1. %s\n2. %s\n4. %s\n5. %s",BD_list_fails[0],BD_list_fails[1],BD_list_fails[3],BD_list_fails[4]);
+
+    do{
+        printf("\nВыберите файл [1,2,4,5]: ");
+        rez=getchar();while(getchar() != '\n'); /*Сбрасываем все символы кроме первого*/
+
+        switch (rez) {
+        case '1':
+            count=0;
+            printf("\n|Номер|Код|Наименование|Возраст|Должность|Оклад|\n");
+            while(rez_out=BD_output(BD_list_fails[0],&E_temp,E_size)){ count++;
+                if(E_temp.code != -1) printf("|%5i|%3i|%12s|%7i|%9s|%5i|\n",rez_out,E_temp.code,E_temp.name,E_temp.age,E_temp.position,E_temp.salary);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            BD_remove(BD_list_fails[0],E_size,rez_out-1);
+            break;
+        case '2':
+            count=0;
+            printf("\n|Номер|Код|Наименование| % |\n");
+            while(rez_out=BD_output(BD_list_fails[1],&N_temp,N_size)){ count++;
+                if(N_temp.code != -1) printf("|%5i|%3i|%12s|%3i|\n",rez_out,N_temp.code,N_temp.name,N_temp.markup_percentage);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            BD_remove(BD_list_fails[1],N_size,rez_out-1);
+            break;
+        case '4':
+            count=0;
+            printf("\n|Номер|Код|Наименование|\n");
+            while(rez_out=BD_output(BD_list_fails[3],&C_temp,C_size)){ count++;
+                if(C_temp.code != -1) printf("|%5i|%3i|%12s|\n",rez_out,C_temp.code,C_temp.name);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            BD_remove(BD_list_fails[3],C_size,rez_out-1);
+            break;
+        case '5':
+            count=0;
+            printf("\n|Номер|Код| Дата |Работник|Статус|\n");
+            while(rez_out=BD_output(BD_list_fails[4],&M_temp,M_size)){ count++;
+                if(M_temp.code != -1) printf("|%5i|%3i|%6i|%8i|%6i|\n",rez_out,M_temp.code,M_temp.data,M_temp.code_employee,M_temp.status);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            printf("Выберите номер строки: "); scanf("%i",&rez_out);
+            BD_remove(BD_list_fails[4],M_size,rez_out-1);
+            break;
+        case '0': return;
+        }
+    }while('1'!=rez && rez!='2' && rez!='4' && rez!='5');
+}
+
+void out_record_to_table(){
+    char rez;
+    int rez_out, count=0;
+    /*Вывод всех файлов*/
+    puts("\nВывод всех записей в таблицах:\nВсе таблицы:");
+    for(int i=0;i<7;i++){
+        printf("%i. %s\n",i+1,BD_list_fails[i]);
+    }
+    do{
+        printf("\nВыберите файл [1-7]: ");
+        rez=getchar();while(getchar() != '\n'); /*Сбрасываем все символы кроме первого*/
+
+        switch (rez) {
+        case '1':
+            printf("\n|Номер|Код|Наименование|Возраст|Должность|Оклад|\n");
+            while(rez_out=BD_output(BD_list_fails[0],&E_temp,E_size)){ count++;
+                if(E_temp.code != -1) printf("|%5i|%3i|%12s|%7i|%9s|%5i|\n",rez_out,E_temp.code,E_temp.name,E_temp.age,E_temp.position,E_temp.salary);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '2':
+            printf("\n|Номер|Код|Наименование| % |\n");
+            while(rez_out=BD_output(BD_list_fails[1],&N_temp,N_size)){ count++;
+                if(N_temp.code != -1) printf("|%5i|%3i|%12s|%3i|\n",rez_out,N_temp.code,N_temp.name,N_temp.markup_percentage);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '3':
+            printf("\n|Номер|Код|Наименование|Вид склада|\n");
+            while(rez_out=BD_output(BD_list_fails[2],&S_temp,S_size)){ count++;
+                if(S_temp.code != -1) printf("|%5i|%3i|%12s|%10s|\n",rez_out,S_temp.code,S_temp.name,S_temp.type_of_warehouse);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '4':
+            printf("\n|Номер|Код|Наименование|\n");
+            while(rez_out=BD_output(BD_list_fails[3],&C_temp,C_size)){ count++;
+                if(C_temp.code != -1) printf("|%5i|%3i|%12s|\n",rez_out,C_temp.code,C_temp.name);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '5':
+            printf("\n|Номер|Код| Дата |Работник|Статус|\n");
+            while(rez_out=BD_output(BD_list_fails[4],&M_temp,M_size)){ count++;
+                if(M_temp.code != -1) printf("|%5i|%3i|%6i|%8i|%6i|\n",rez_out,M_temp.code,M_temp.data,M_temp.code_employee,M_temp.status);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '6':
+            printf("\n|Номер|Код| Дата |Контрагент|Склад|Сумма|\n");
+            while(rez_out=BD_output(BD_list_fails[5],&D_temp,D_size)){ count++;
+                if(D_temp.code != -1) printf("|%5i|%3i|%6i|%10i|%5i|%5i|\n",rez_out,D_temp.code,D_temp.data,D_temp.code_counterparties,D_temp.code_stock,D_temp.summa);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '7':
+            printf("\n|Номер|Код|Товар|Склад|Количество|Сумма|\n");
+            while(rez_out=BD_output(BD_list_fails[6],&L_temp,L_size)){ count++;
+                if(L_temp.code != -1) printf("|%5i|%3i|%5i|%5i|%10i|%5i|\n",rez_out,L_temp.code,L_temp.code_nomenclature,L_temp.code_stock,L_temp.quantity,L_temp.summa);
+            }
+            if(!count) {printf("Нет записей\n"); return;}
+            break;
+        case '0': return;
+        }
+    }while('1'>rez || rez>'7');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
